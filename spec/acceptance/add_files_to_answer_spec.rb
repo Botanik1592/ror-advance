fac2617c3cb8f379f84b22500de5615e715ffbd8
Botanik1592 <anton.buryka@gmail.com>
2016-11-02 16:24:23 +0300

    User can add many files to answer

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -17,14 +17,21 @@ feature 'Add files to answer', %q{
   scenario "User add file when give answer", js: true do
     fill_in 'new-answer-body', with: 'test_test_test'
 
-    within '.nested-form' do
+    within all('.nested-form').last do
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'Add file'
+
+    within all('.nested-form').first do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
     end
 
     click_on 'Create answer'
 
     within '.one-answer' do
-      expect(page).to have_link "spec_helper.rb", href: "/uploads/attachment/file/1/spec_helper.rb"
+      expect(page).to have_link "spec_helper.rb", href: "/uploads/attachment/file/2/spec_helper.rb"
+      expect(page).to have_link "rails_helper.rb", href: "/uploads/attachment/file/1/rails_helper.rb"
     end
   end
 end
