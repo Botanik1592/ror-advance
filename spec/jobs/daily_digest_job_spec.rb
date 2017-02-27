3aa31c6f3372365be6a5a7c3a1538babbc031707
Botanik1592 <anton.buryka@gmail.com>
2017-02-27 15:49:52 +0300

    Fix spec

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
+  let!(:users) { create_list(:user, 3) }
 
   it 'sends daily digest' do
-    User.find_each.each do
+    users.each do |user|
       expect(DailyMailer).to receive(:digest).with(user).and_call_original
     end
     DailyDigestJob.perform_now
