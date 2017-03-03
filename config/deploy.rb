c7b54f21d570e2e3dbe48e035d2712659258733c
Botanik1592 <anton.buryka@gmail.com>
2017-03-03 18:45:45 +0300

    sphinx

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ append :linked_dirs, "log",
                      "tmp/sockets",
                      "public/system",
                      'public/uploads',
-                     'vendor/bundle'
+                     'db/sphinx'
 
 namespace :deploy do
 
